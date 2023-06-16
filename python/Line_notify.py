24b17ebc13baef06b87e9a3f584d22468a1645be
ChenBingWei1201 <rain4030633@gmail.com>
2023-06-16 22:51:48 +0800

    modified

--- python/Line_notify.py
+++ python/Line_notify.py
@@ -1,7 +1,6 @@
 import requests
 import time
 def Warning(self,time,picurl):
-def Warning(self,time,picurl):
     headers = {
         "Authorization": "Bearer " + "OWbgpbxq30zMuNAzDoaf9HeSiTP0t3hEodgeWs0hR19",
         "Content-Type": "application/x-www-form-urlencoded"
@@ -9,9 +8,7 @@ def Warning(self,time,picurl):
  
     params = {"message": "Your RobotVacuum detected someone fainted at" + time + ", please confirm his/her safety as soon as possible! "}
     files = {"imageFile": open(picurl, "rb")}
-    files = {"imageFile": open(picurl, "rb")}
     r = requests.post("https://notify-api.line.me/api/notify",
-                      headers=headers, params=params, files=files)
                       headers=headers, params=params, files=files)
     print(r.status_code)  #200
 """
