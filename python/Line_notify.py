7566044f014c2a0c28fdbe26cc0a3728128ecd3a
ChenBingWei1201 <rain4030633@gmail.com>
2023-06-21 18:12:24 +0800

    send picture

--- python/Line_notify.py
+++ python/Line_notify.py
@@ -2,21 +2,26 @@ import requests
 import time
 import serial
 
-def Warning(self,time,picurl):
-    headers = {                     
-        "Authorization": "Bearer " + "OWbgpbxq30zMuNAzDoaf9HeSiTP0t3hEodgeWs0hR19",
-       # "Content-Type": "application/x-www-form-urlencoded"
+import requests
+    # 這邊加上picurl
+def lineNotify(token, msg, picurl):
+    # line伺服器位址
+    url = "https://notify-api.line.me/api/notify"
+    # token認證用
+    headers = {
+        "Authorization": "Bearer " + "OWbgpbxq30zMuNAzDoaf9HeSiTP0t3hEodgeWs0hR19"
     }
     
-    Buzzer() # bee~ bee~ bee~
+    Buzzer() # beep
     
-    params = {"message": "Your RobotVacuum detected someone fainted at" + time + ", please confirm his/her safety as soon as possible! "}
-    files = {"imageFile": open(picurl, "rb")}
-    s = requests.post("https://notify-api.line.me/api/notify", headers=headers, params=params)
-    print(s.status_code)
-    r = requests.post("https://notify-api.line.me/api/notify", headers=headers, params="picture:", files=files)
-    print(r.status_code)  #200
-    return 0
+    # 宣告一個物件，裡面存放要傳送的訊息
+    payload = { 'message': msg,}
+    files = {'imageFile': open(picurl, 'rb')}
+    # 將headers和data傳送至url，也就是將token和訊息傳送至line伺服器
+    r = requests.post(url, headers=headers,  paramas=payload, files=files)
+    # status_code為requests除錯用，回傳錯誤代碼，不用理會
+    print(r.status_code)
+    return r.status_code
 
 # use rpi to send message to arduino with USB line and the buzzer will bee when someone faints
 def Buzzer():
